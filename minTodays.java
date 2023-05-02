b0dad4b365e7a22ed8b93c5c1a6da8af7e17fedf
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-02 18:22:49 +0530

    Add files via upload

---
+++ minTodays.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class minTodays {
+   /*java program to print number of minutes into years and days*/
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.print("INPUT NUMBER OF MINUTES : ");
+		int min=Integer.parseInt(sc.nextLine());
+        int yr=min/(60*24*365);
+        int day=(min/1440)-(yr*365);
+        System.out.println(+min+" minutes is approximately \n"+yr+" years and "+day+" days");
+	}
+
+}

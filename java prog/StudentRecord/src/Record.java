e6c1729fbc9c218300223de90cfb437ac6470323
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-03 23:15:31 +0530

    Update Record.java

--- java prog/StudentRecord/src/Record.java
+++ java prog/StudentRecord/src/Record.java
@@ -4,7 +4,6 @@ public class Record {
 public static void main(String a[])
 {
 	Scanner sc=new Scanner(System.in);
-	
 	System.out.println("STUDENT RECORD MANAGEMNT SYSTEM");
 	Student s[]=new Student[12];
 	int st=0;

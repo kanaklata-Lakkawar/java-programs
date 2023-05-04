ed58d99cb1699bf627c6aee3339338002a3da440
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-04 09:41:45 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Test1 {
+    /* java program that reads an integer between 0 and 1000
+      and adds all the digit in the number */
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+    System.out.println("INPUT AN INTEGER BETWEEN 0 AND 100 : ");
+	int num=Integer.parseInt(sc.nextLine());
+	int rem=0,val=0;
+	while(num!=0)
+	{ 
+		rem=num%10;
+		val+=rem;
+		num/=10;
+	}
+	System.out.println("THE SUM OF ALL DIGITS IN number is\n"+val);
+	}
+}

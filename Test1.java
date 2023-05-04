d4b82e862f03ceb7d94a81ccd4c0e4963907d882
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-04 09:42:07 +0530

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,8 +1,7 @@
 import java.util.Scanner;
 
 public class Test1 {
-    /* java program that reads an integer between 0 and 1000
-      and adds all the digit in the number */
+   
 	public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
     System.out.println("INPUT AN INTEGER BETWEEN 0 AND 100 : ");

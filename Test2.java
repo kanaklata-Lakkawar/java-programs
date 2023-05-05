2680f7e66eb170ddfef0854772c66005eb893924
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-05 09:59:28 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Test2 {
+public static void main(String[] args)
+{
+	/* Java program to break an integer into a 
+	sequence of individual digits.*/
+	Scanner sc=new Scanner(System.in);
+	System.out.println("input the six non-negative digits : ");
+	int num=Integer.parseInt(sc.nextLine());
+	num%=10;
+	System.out.println(num);
+}
+}

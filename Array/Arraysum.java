ff62061ae2e30be45ed99a9bf0c93bc02a5d42f1
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-09 23:14:24 +0530

    Update Arraysum.java

--- Array/Arraysum.java
+++ Array/Arraysum.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
-
+/Write a Java program to sum values of an array*/
 public class Arraysum {
-/*Write a Java program to sum values of an array?*/
+
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("enter size of an array : ");

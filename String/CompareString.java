535381b8357816dafa0856924a7a2cda4594e109
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-19 19:09:50 +0530

    Add files via upload

---
+++ String/CompareString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+/*java program to compare two strings.*/
+public class CompareString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER STRING 1 : ");
+		String s1=(sc.nextLine());
+		System.out.println("ENTER STRING 2 : ");
+		String s2=(sc.nextLine());
+	if((s1.compareTo(s2))<0)
+		System.out.println(s1+" is less than "+s2);
+	if((s1.compareTo(s2))>0)
+		System.out.println(s1+" is greater than "+s2);
+	}
+
+}

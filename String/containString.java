fda3e57d3f1e1c902dae5395e41f15cb9650fcba
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-05-25 10:24:47 +0530

    Add files via upload

---
+++ String/containString.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class containString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER STRING : ");
+		String s=(sc.nextLine());
+		String s1="and";
+		System.out.println(s.contains(s1));
+	
+	}
+
+}

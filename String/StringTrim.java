f0fd97c23c40a68ae8dd7e5ea3b015386e29b270
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-06-10 08:45:11 +0530

    Add files via upload

---
+++ String/StringTrim.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringTrim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any string");
+		String s=(sc.nextLine());
+		System.out.println(s.trim());
+	}
+
+}

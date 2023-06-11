9c4420df4dac39b55ddab2d0b3e71eaa403a1373
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-06-11 08:03:04 +0530

    Add files via upload

---
+++ String/StringUppercase.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringUppercase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER ANY STRING : ");
+		String s=(sc.nextLine());
+		System.out.println(s.toUpperCase());
+	}
+
+}

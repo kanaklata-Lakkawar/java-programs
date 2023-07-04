7010160cee620bb1b0af31787a3fe5a45b767159
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-07-04 07:47:09 +0530

    Add files via upload

---
+++ ArrayList/que4/Test.java
@@ -0,0 +1,20 @@
+package que4;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> str=new ArrayList<String>();
+		str.add("Red");
+		str.add("Green");
+		str.add("Orange");
+		str.add("White");
+		str.add("Black");
+		System.out.println(str);
+	System.out.println("First element : "+str.get(0));
+		System.out.println("Third element : "+str.get(2));
+
+	}
+
+}

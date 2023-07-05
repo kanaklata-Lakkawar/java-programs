1f7ad4036ccfe7979a05c8803402880a80d8c680
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-07-05 08:46:07 +0530

    Add files via upload

---
+++ ArrayList/que5/Test.java
@@ -0,0 +1,18 @@
+package que5;
+
+import java.util.ArrayList;
+
+public class Test {
+	
+public static void main(String[] args){
+	ArrayList<String> str=new ArrayList<String>();
+	str.add("Red");
+	str.add("Green");
+	str.add("Orange");
+	str.add("White");
+	str.add("Black");
+	System.out.println(str);
+	str.set(2, "Yellow");
+	System.out.println(str);
+}
+}

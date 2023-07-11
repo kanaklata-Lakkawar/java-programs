9997b1c67f471ed9a3d07a295b589ac0a772dc8d
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-07-11 08:04:37 +0530

    Add files via upload

--- ArrayList/Test.java
+++ ArrayList/Test.java
@@ -1,24 +1,19 @@
-package que7;
+package que8;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
 		ArrayList<String> str=new ArrayList<String>();
-		Scanner sc=new Scanner(System.in);
-str.add("Red");
-str.add("Green");
-str.add("Orange");
-str.add("White");
-str.add("Black");
-System.out.println(str);
-System.out.println("Enter element you want to find : ");
-String s=sc.next();
-
-if(str.contains(s))
-	System.out.println("Found the element");
-	System.out.println("This element is not in the List");}
-	
+		str.add("Red");
+		str.add("Green");
+		str.add("Orange");
+		str.add("White");
+		str.add("Black");
+		System.out.println(str);
+		str.sort(null);
+		System.out.println(str);
 	}
+
+}

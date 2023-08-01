a829dab1b1e573fbba74fb141ffd5d01841d9447
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-01 09:24:22 +0530

    Add files via upload

---
+++ ArrayList/que20/Test.java
@@ -0,0 +1,19 @@
+package que20;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ArrayList<String> str=new ArrayList<String>();
+		str.add("Red");
+		str.add("Green");
+		str.add("Black");
+		System.out.println("Original array : "+str);
+		str.add("White");
+		str.add("Pink");
+		str.add("Yellow");
+		System.out.println("New arrayList : "+str);
+		}
+
+}

c8449516fbb4c35446ec84780528fb66c5821c45
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-05 20:40:02 +0530

    Add files via upload

---
+++ LiinkedList/que1/Test.java
@@ -0,0 +1,20 @@
+package que1;
+
+import java.util.LinkedList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedList<String> str=new LinkedList<String>();
+		str.add("Red");
+		str.add("Green");
+		str.add("Black");
+		str.add("White");
+		str.add("Pink");
+		System.out.println(str);
+		str.add("Yellow");
+		System.out.println(str);
+		
+	}
+
+}

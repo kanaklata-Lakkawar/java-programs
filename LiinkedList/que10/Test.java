641a52615e958757bc0ed8950178692abb4a2a99
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-11 08:14:14 +0530

    Add files via upload

---
+++ LiinkedList/que10/Test.java
@@ -0,0 +1,20 @@
+package que10;
+
+import java.util.LinkedList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedList<String>str=new LinkedList<String>();
+		str.add("Red");
+		str.add("Green");
+		str.add("Black");
+		str.add("Pink");
+		str.add("Orange");
+		System.out.println(str);
+        System.out.println("First elements is : "+str.getFirst());
+        System.out.println("Last element ism : "+str.getLast());
+		
+	}
+
+}

1f40858a5cd196af0eeef0a3b128d214906b72df
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-24 09:18:41 +0530

    Add files via upload

---
+++ HashSet/que1/Test.java
@@ -0,0 +1,21 @@
+package que1;
+
+import java.util.HashSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HashSet<String> h=new HashSet<String>();
+		h.add("Red");
+		h.add("White");
+		h.add("Pink");
+		h.add("yellow");
+		h.add("Black");
+		h.add("Green");
+		System.out.println(h);
+		
+		
+
+	}
+
+}

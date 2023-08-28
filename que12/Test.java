143579c73334d35761f34129c1562adc30cd10a0
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-28 13:15:26 +0530

    Add files via upload

---
+++ que12/Test.java
@@ -0,0 +1,18 @@
+package que12;
+
+import java.util.HashSet;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HashSet<String> h=new HashSet<String>();
+		h.add("Red");
+		h.add("White");
+		h.add("Black");
+		h.add("Green");
+		System.out.println("HashSet Contents :"+h);
+		h.clear();
+		System.out.println("HashSet Contents :"+h);
+	}
+
+}

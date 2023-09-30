c08e8397dad8920e24ce2175d02d2d4885829023
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-09-30 09:05:06 +0530

    Add files via upload

---
+++ Map/TreeMap/que17/Test.java
@@ -0,0 +1,20 @@
+package que17;
+
+import java.util.TreeMap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		TreeMap<Integer,String> t=new TreeMap<Integer,String>();
+		t.put(10, "Red");
+		t.put(20, "Green");
+		t.put(40, "Black");
+		t.put(50, "White");
+		t.put(60, "Pink");
+		System.out.println("Value before poll : "+t);
+		System.out.println("Value returned : "+t.pollFirstEntry());
+		System.out.println("Value after poll : "+t);
+
+	}
+
+}

e14f0a7c3b14f32bcfd5cc7ac3af0b5edfd48d93
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-11-10 08:46:25 +0530

    Add files via upload

---
+++ Servlet/S-Day4/src/MyServ2.java
@@ -0,0 +1,41 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServ
+ */
+@WebServlet("/serv")
+public class MyServ2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String s=request.getParameter("uname");
+        out.print("Welcome  "+s);
+        
+        Cookie c=new Cookie("uname",s);
+        response.addCookie(c);
+        
+        out.println("<form action='serv1'method ='get'>");
+        out.println("<input type='submit' value='go'");
+        out.println("</form>");
+        
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}

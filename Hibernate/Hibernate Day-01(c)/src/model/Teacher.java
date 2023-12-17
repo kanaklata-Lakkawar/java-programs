0a563e1006ed3c6ee57c8b565ef6f12e0653ac8f
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-12-17 19:28:59 +0530

    Add files via upload

---
+++ Hibernate/Hibernate Day-01(c)/src/model/Teacher.java
@@ -0,0 +1,56 @@
+package model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+public class Teacher {
+	
+private int id;
+
+private String name;
+private String sub;
+
+
+
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getSub() {
+	return sub;
+}
+public void setSub(String sub) {
+	this.sub = sub;
+}
+public Teacher() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+public Teacher(int id, String name, String sub) {
+	super();
+	this.id = id;
+	this.name = name;
+	this.sub = sub;
+}
+@Override
+public String toString() {
+	return "Teacher [id=" + id + ", name=" + name + ", sub=" + sub + "]";
+}
+
+
+
+}

5c1a01657a83912235c3bab918fc4e88bd0d9844
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-12-29 09:00:09 +0530

    Add files via upload

---
+++ java prog/7_Swap/src/Swap.java
@@ -0,0 +1,21 @@
+
+public class Swap {
+
+	public static void main(String[] args) {
+		int x=78;
+		int y=65;
+		
+		System.out.println("Before : x : "+x+"  y : "+y);
+	
+
+		x=x+y;
+		
+		y=x-y;
+		
+		x=x-y;
+		 System.out.println("After : x : "+x+"  y : "+y);
+		
+		
+	}
+
+}

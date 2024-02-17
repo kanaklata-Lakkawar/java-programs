85c0a77b5844e911d292ce1ab709345171d9b8c0
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2024-02-17 09:20:43 +0530

    Add files via upload

---
+++ Spring/SP-JDBC/src/main/java/com/programming/springboot/SpJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.programming.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpJdbcApplication.class, args);
+	}
+
+}
---
+++ Spring/SP-JDBC/src/test/java/com/programming/springboot/SpJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

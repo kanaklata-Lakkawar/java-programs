f9ddd12454e24d47b41cd913a3bb707fe91fef57
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2024-02-18 11:19:31 +0530

    Add files via upload

---
+++ Spring/SpDay08/src/in/mypack/model/Student.java
@@ -0,0 +1,11 @@
+package in.mypack.model;
+
+public class Student {
+
+	
+	private int id;
+	private String name;
+	private String addr;
+	
+	
+}
---
+++ Spring/SpDay08/src/main/java/com/example/demo/SpDay08Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpDay08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpDay08Application.class, args);
+	}
+
+}

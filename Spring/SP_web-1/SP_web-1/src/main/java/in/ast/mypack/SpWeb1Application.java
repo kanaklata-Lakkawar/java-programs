d729dcda891db8676b2fbcb23ffd8e6e900d9d9e
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2024-03-14 11:56:21 +0530

    Add files via upload

---
+++ Spring/SP_web-1/SP_web-1/src/main/java/in/ast/mypack/SpWeb1Application.java
@@ -0,0 +1,13 @@
+package in.ast.mypack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWeb1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWeb1Application.class, args);
+	}
+
+}

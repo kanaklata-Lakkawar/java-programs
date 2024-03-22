2436d221e2199ab6836805c33ecccf83876a90a0
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2024-03-22 09:30:18 +0530

    Add files via upload

---
+++ Spring/SP_web-2/src/main/java/in/ast/mypack/SpWeb2Application.java
@@ -0,0 +1,13 @@
+package in.ast.mypack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpWeb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpWeb2Application.class, args);
+	}
+
+}
---
+++ Spring/SP_web-2/src/test/java/in/ast/mypack/SpWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ast.mypack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
